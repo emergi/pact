e5bba55dcd7e4a84396d8927399bca253f526708
samuelweckstrom <samuel.weckstrom@gmail.com>
2015-10-05 18:22:43 +0300

    Added sticky headers to mobile site.

--- js.js
+++ js.js
@@ -15,25 +15,25 @@ $(function() {
 
 $(function() {
 
-$("#paulDetail").click(function() {
-$("#paulInfo").fadeIn();
-});
+	$("#paulDetail").click(function() {
+		$("#paulInfo").fadeIn();
+	});
 
-$("#jerryDetail").click(function() {
-$("#jerryInfo").fadeIn();
-});
+	$("#jerryDetail").click(function() {
+		$("#jerryInfo").fadeIn();
+	});
 
-$("#janDetail").click(function() {
-$("#janInfo").fadeIn();
-});
+	$("#janDetail").click(function() {
+		$("#janInfo").fadeIn();
+	});
 
-$("#keithDetail").click(function() {
-$("#keithInfo").fadeIn();
-});
+	$("#keithDetail").click(function() {
+		$("#keithInfo").fadeIn();
+	});
 
-$(".overlay").click(function() {
-$("#paulInfo, #jerryInfo, #janInfo, #keithInfo").fadeOut();
-});
+	$(".overlay").click(function() {
+		$("#paulInfo, #jerryInfo, #janInfo, #keithInfo").fadeOut();
+	});
 
 });
 
@@ -140,6 +140,121 @@ else {
 //     return window.pageYOffset || document.documentElement.scrollTop;
 //     }
 // });
+
+
+
+
+
+
+
+
+
+
+// Sticky-header
+
+
+
+var stickyHeaders = (function() {
+
+  var $window = $(window),
+      $stickies;
+
+	var load = function(stickies) {
+
+	    if (typeof stickies === "object" && stickies instanceof jQuery && stickies.length > 0) {
+
+			$stickies = stickies.each(function() {
+				var $thisSticky = $(this).wrap('<div class="stickyWrap" />');
+				$thisSticky
+				    .data('originalPosition', $thisSticky.offset().top)
+				    .data('originalHeight', $thisSticky.outerHeight())
+				    .parent()
+				    .height($thisSticky.outerHeight()); 			  
+			});
+
+	      $window.off("scroll.stickies").on("scroll.stickies", function() {
+			  _whenScrolling();		
+	      });
+	    }
+ 	};
+
+	var _whenScrolling = function() {
+
+		$stickies.each(function(i) {			
+
+			var $thisSticky = $(this),
+			$stickyPosition = $thisSticky.data('originalPosition');
+
+			if ($stickyPosition <= $window.scrollTop()) {        
+
+			    var $nextSticky = $stickies.eq(i + 1),
+			        $nextStickyPosition = $nextSticky.data('originalPosition') - $thisSticky.data('originalHeight');
+
+			    $thisSticky.addClass("fixed");
+
+			    if ($nextSticky.length > 0 && $thisSticky.offset().top >= $nextStickyPosition) {
+			    $thisSticky.addClass("absolute").css("top", $nextStickyPosition);
+			    }
+
+			} else {
+
+			    var $prevSticky = $stickies.eq(i - 1);
+
+			    $thisSticky.removeClass("fixed")
+
+			    if ($prevSticky.length > 0 && $window.scrollTop() <= $thisSticky.data('originalPosition') - $thisSticky.data('originalHeight')) {
+			    $prevSticky.removeClass("absolute").removeAttr("style");
+			    }
+			}
+		});
+	};
+
+	return {
+		load: load
+	};
+})();
+
+
+$(function() {
+  stickyHeaders.load($(".sticky-header"));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
 
 
@@ -152,5 +267,4 @@ jQuery.extend( jQuery.easing,
     easeInOutSine: function(x, t, b, c, d) {
         return -c / 2 * (Math.cos(Math.PI * t / d) - 1) + b;
     }
-
 });
