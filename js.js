0de2af4ba601f7d12e1c636557f2f7d372cea874
samuelweckstrom <samuel.weckstrom@gmail.com>
2015-10-08 17:20:32 +0300

    scroll height adjusted for mobile test 6

--- js.js
+++ js.js
@@ -154,10 +154,10 @@ for (var i = stickyElements.length - 1; i >= 0; i--) {
 
 
 var shrinkLogo = 150;
-var shrinkPaul = 6700;
-var shrinkJan	= 8100;
-var shrinkKeith = 9500;
-var shrinkJerry = 10900;
+var shrinkPaul = 6820;
+var shrinkJan	= 8220;
+var shrinkKeith = 9620;
+var shrinkJerry = 11020;
 var endProfile = 12660;
 
 
